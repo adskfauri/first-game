57a0244f4304a874daadb88f18ef1bc815f5fc84
adskfauri <219985622+adskfauri@users.noreply.github.com>
2026-07-31 13:14:23 +0900

    Add complete crime party game

--- game-2/firebase-config.js
+++ game-2/firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase 콘솔에서 프로젝트를 만든 뒤 아래 값을 교체하세요.
 // Realtime Database를 활성화해야 여러 명이 동시에 접속할 수 있습니다.
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCTy47ng7rULuj3S9WoICzw4RSnCEwP0rc",
+  authDomain: "first-project-a8f86.firebaseapp.com",
+  projectId: "first-project-a8f86",
+  storageBucket: "first-project-a8f86.firebasestorage.app",
+  messagingSenderId: "755450543732",
+  appId: "1:755450543732:web:dcf16a3c7021209c925b21",
+  measurementId: "G-8XBKZZVNE2"
 };
